47ab2046c35ae82842c153ad6e660136ad4a4212
陳柏瑋 <snfr067@gmail.com>
2022-02-07 09:17:10 +0800

    Init

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace EveryTeacher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_select_file = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lb_sys_info = new System.Windows.Forms.Label();
+            this.lb_load_data_count = new System.Windows.Forms.Label();
+            this.btn_select_sheet = new System.Windows.Forms.Button();
+            this.text = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_select_file
+            // 
+            this.btn_select_file.Location = new System.Drawing.Point(12, 22);
+            this.btn_select_file.Name = "btn_select_file";
+            this.btn_select_file.Size = new System.Drawing.Size(89, 29);
+            this.btn_select_file.TabIndex = 0;
+            this.btn_select_file.Text = "按下";
+            this.btn_select_file.UseVisualStyleBackColor = true;
+            this.btn_select_file.Click += new System.EventHandler(this.btn_select_file_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(268, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(520, 426);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // lb_sys_info
+            // 
+            this.lb_sys_info.AutoSize = true;
+            this.lb_sys_info.Location = new System.Drawing.Point(13, 238);
+            this.lb_sys_info.Name = "lb_sys_info";
+            this.lb_sys_info.Size = new System.Drawing.Size(41, 15);
+            this.lb_sys_info.TabIndex = 4;
+            this.lb_sys_info.Text = "label1";
+            // 
+            // lb_load_data_count
+            // 
+            this.lb_load_data_count.AutoSize = true;
+            this.lb_load_data_count.Location = new System.Drawing.Point(16, 287);
+            this.lb_load_data_count.Name = "lb_load_data_count";
+            this.lb_load_data_count.Size = new System.Drawing.Size(41, 15);
+            this.lb_load_data_count.TabIndex = 5;
+            this.lb_load_data_count.Text = "label1";
+            // 
+            // btn_select_sheet
+            // 
+            this.btn_select_sheet.Location = new System.Drawing.Point(16, 78);
+            this.btn_select_sheet.Name = "btn_select_sheet";
+            this.btn_select_sheet.Size = new System.Drawing.Size(75, 23);
+            this.btn_select_sheet.TabIndex = 6;
+            this.btn_select_sheet.Text = "button1";
+            this.btn_select_sheet.UseVisualStyleBackColor = true;
+            this.btn_select_sheet.Click += new System.EventHandler(this.btn_select_sheet_Click);
+            // 
+            // text
+            // 
+            this.text.AutoSize = true;
+            this.text.Location = new System.Drawing.Point(16, 334);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(41, 15);
+            this.text.TabIndex = 7;
+            this.text.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.text);
+            this.Controls.Add(this.btn_select_sheet);
+            this.Controls.Add(this.lb_load_data_count);
+            this.Controls.Add(this.lb_sys_info);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_select_file);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_select_file;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lb_sys_info;
+        private System.Windows.Forms.Label lb_load_data_count;
+        private System.Windows.Forms.Button btn_select_sheet;
+        private System.Windows.Forms.Label text;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Diagnostics;
+using DataTable = System.Data.DataTable;
+
+namespace EveryTeacher
+{
+    public partial class Form1 : Form
+    {
+
+        Excel.Application _xlApp;
+        Excel.Workbook _xlWorkBook;
+        Excel.Worksheet _xlWorkSheet;
+        Excel.Range _range;
+        String _file_name = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //指定Excel檔案路徑
+            string str = System.Windows.Forms.Application.StartupPath + "\\input_file";
+            //-----------------------下拉選單製作
+            
+        }
+        private void btn_select_file_Click(object sender, EventArgs e)
+        {
+            _file_name = ImportExcelFile();
+            //開啟Excel
+            _xlApp = new Excel.Application();
+            _xlWorkBook = _xlApp.Workbooks.Open(_file_name, 0, true, 5, "", "", true,
+              Microsoft.Office.Interop.Excel.XlPlatform.xlWindows
+             , "\t", false, false, 0, true, 1, 0);
+            //-----------------------worksheet下拉選單
+            Excel.Worksheet xlWorkSheet2;
+            lb_sys_info.Text = "Excel工作表載入中";
+            for (int i = 1; i <= _xlWorkBook.Sheets.Count; i++) //計算總共有幾個工作表
+            {
+                //取得該工作表
+                xlWorkSheet2 = (Excel.Worksheet)_xlWorkBook.Worksheets[i];
+                //將該工作表名稱加入下拉選單
+               
+            }
+            lb_sys_info.Text = "Excel工作表載入完成";
+            //使用完EXCEL資源釋放
+            _xlApp.Quit();
+            Form1.KillExcelApp(_xlApp);
+        }
+        private System.Data.DataTable _tb = new System.Data.DataTable("table");
+        DataRow _NewRow;
+        Thread _read_execel_data;
+        private void btn_select_sheet_Click(object sender, EventArgs e)
+        {
+            lb_load_data_count.Visible = true;
+            _read_execel_data = new Thread(read_execel_data);
+            _read_execel_data.Start();
+        }
+        private void read_execel_data()
+        {
+            string[] teachers;
+            int teacherIndex = 0;
+            int teacherCount = 0;
+            int wantToReadRows = 200;
+            int select_sheet = 0;  //select_sheet第幾個工作表
+            string str = System.Windows.Forms.Application.StartupPath;
+            //開啟Excel
+            _xlApp = new Excel.Application();
+            this.Invoke((MethodInvoker)delegate
+            {
+                dataGridView1.DataSource = null;
+                _tb.Clear(); //清空表格
+                _tb.Rows.Clear();//清空資料
+                _tb.Columns.Clear();
+            });
+            try
+            {
+                _xlWorkBook = _xlApp.Workbooks.Open(_file_name, 0, true, 5, "", "", true,
+                 Microsoft.Office.Interop.Excel.XlPlatform.xlWindows
+                  , "\t", false, false, 0, true, 1, 0);
+            }
+            catch
+            {
+                MessageBox.Show("未選擇檔案");
+                return;
+            }
+            //取得下拉選單數值讀取指定工作表
+            this.Invoke((MethodInvoker)delegate
+            {                
+                select_sheet = 1;
+                _xlWorkSheet = (Excel.Worksheet)_xlWorkBook.Worksheets[select_sheet];
+                _range = _xlWorkSheet.UsedRange;//讀取Excel列與行資訊
+            });
+            int rw = _range.Rows.Count;     //直 (總行數)
+            int cl = _range.Columns.Count;  //橫 (總列數)
+            int sum = rw;
+            DataColumn[] colItem = new DataColumn[cl + 1];
+            String[] title = new String[cl + 1];
+            Excel.Worksheet excelSheet =
+           (Excel.Worksheet)_xlWorkBook.Worksheets[select_sheet];
+            //-----------------------------表格欄位製作
+            for (int i = 1; i <= cl; i++)
+            {
+                //將Table Titile塞入Excel第一列作為標題 [1,n]
+                Excel.Range rng = (Excel.Range)excelSheet.Cells[1, i];
+                try
+                {
+                    colItem[i] = new DataColumn(rng.Value, Type.GetType("System.String"));
+                    title[i] = rng.Value;
+                }
+                catch
+                {
+                    colItem[i] = new DataColumn(" ", Type.GetType("System.String"));
+                }
+                _tb.Columns.Add(colItem[i]);
+            }
+            sum = rw - 1;
+            lb_sys_info.Invoke((MethodInvoker)delegate
+            {
+                lb_sys_info.Text = "Excel工作表內容載入中";
+            });
+
+            teachers = new string[wantToReadRows];
+
+            for (int i = 2; i <= wantToReadRows; i++)
+            {
+                lb_load_data_count.Invoke((MethodInvoker)delegate
+                {
+                    lb_load_data_count.Text =
+                    "尚有 " + (wantToReadRows - i - 1).ToString() + " 筆資料未加入";
+                });
+
+                
+                _NewRow = _tb.NewRow(); //開新的一列
+                for (int j = 1; j <= cl; j++)
+                {
+                    try
+                    {
+                        Excel.Range rng = (Excel.Range)excelSheet.Cells[i, j];
+                        //讀取 Excel每格內容
+                        _NewRow[title[j]] = rng.Value.ToString(); //將取得的值存入陣列
+                    }
+                    catch
+                    {
+                        _NewRow[title[j]] = "";
+                    }
+                }
+
+               text.Invoke((MethodInvoker)delegate
+                {
+                    try
+                    {
+                        Excel.Range rng = (Excel.Range)excelSheet.Cells[i, 4];
+                        text.Text = "("+ i + ")" +rng.Value.ToString(); //將取得的值存入陣列
+
+                        for(teacherIndex = 0; teacherIndex < teacherCount; teacherIndex++)
+                        {
+                            if (teachers[teacherIndex] == rng.Value.ToString())
+                            {
+                                //addNewTeacher = false;
+                                break;
+                            }
+                            /*else
+                            {
+                                addNewTeacher = true;
+                            }*/
+                        }
+                        if(teacherIndex == teacherCount)
+                        {
+                            teachers[teacherCount] = rng.Value.ToString();
+                            teacherCount++; 
+                        }
+
+                      
+                    }
+                    catch (Exception ex)
+                    {
+                        text.Text = ex.Message.ToString();
+                    }
+                });
+                
+
+
+                this.Invoke((MethodInvoker)delegate
+                {
+                    _tb.Rows.Add(_NewRow);
+                });
+            }
+            this.Invoke((MethodInvoker)delegate
+            {
+                _tb.AcceptChanges();
+                dataGridView1.AutoSizeColumnsMode
+                = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+                dataGridView1.DataSource = _tb;
+                lb_load_data_count.Visible = false;
+            });
+
+            text.Invoke((MethodInvoker)delegate
+            {
+                string allTeachers = "";
+                foreach(string teacher in teachers)
+                {
+                    allTeachers += teacher + "\n";
+                }
+                text.Text = allTeachers;
+            });
+
+            //使用完EXCEL資源釋放
+            _xlApp.Quit();
+            Form1.KillExcelApp(_xlApp);
+            lb_sys_info.Invoke((MethodInvoker)delegate
+            {
+                lb_sys_info.Text = "Excel工作表內容載入完成";
+            });
+            _read_execel_data.Abort();
+        }
+        // release excel resource
+        [DllImport("User32.dll")]
+        public static extern int GetWindowThreadProcessId
+        (IntPtr hWnd, out int ProcessId);
+        public static void KillExcelApp(Excel.Application app)
+        {
+            if (app != null)
+            {
+                try
+                {
+                    app.Quit();
+                    IntPtr intptr = new IntPtr(app.Hwnd);
+                    var ps = Process.GetProcessesByName("EXCEL").ToList();
+                    int id;
+                    GetWindowThreadProcessId(intptr, out id);
+                    var p = Process.GetProcessById(id);
+                    //if (p != null)
+                    p.Kill();
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+
+        public String ImportExcelFile()
+        {
+            string windowFilter = "Excel files|*.xlsx";
+            string windowTitle = "匯入Excel資料";
+
+            OpenFileDialog openFileDialogFunction = new OpenFileDialog();
+            openFileDialogFunction.Filter = windowFilter; //開窗搜尋副檔名
+            openFileDialogFunction.Title = windowTitle; //開窗標題
+
+            DataTable dataTable = new DataTable();
+
+            if (openFileDialogFunction.ShowDialog() == DialogResult.OK)
+            {
+                string FilePath = openFileDialogFunction.FileName;
+                return FilePath;
+            }
+
+            return "";
+        }
+    }
+}
